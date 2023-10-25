d8ec6779bbeab9b5e65f7ff19c77806003d56f4e
Klainus <106882227+Klainus@users.noreply.github.com>
2023-10-25 10:25:51 +0200

    Update Register.java
    commented hashSet as it isn't currently used, keeping it for now in case of errors

--- Register.java
+++ Register.java
@@ -1,10 +1,8 @@
-import java.util.HashSet;
-import java.util.Set;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Register {
-    private Set<Integer> existingAccounts = new HashSet<>();
     private Map<Integer, Double> kontoSaldo = new HashMap<>();
 
     public boolean doesAccountExist(int kontoNr) {
